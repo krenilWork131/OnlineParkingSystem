058cbe56bc49ef89e4f9175e0ea544af7f0ece09
krenilWork131 <120437505+krenilWork131@users.noreply.github.com>
2023-08-22 15:30:00 +0530

    changes

--- OnlineParkingSystem/Server/routes/payment.js
+++ OnlineParkingSystem/Server/routes/payment.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const PUBLISHABLE_KEY =
-  "pk_test_51MDtzOBsZ5DeknzbtSoKvfAOpgY0HUmm5ZVCmaIFZgcjE0Qbkz5Zum7j3TEfbq8RO49VCBF7z7tmU3l5pKiklDTf00idaiIlS0";
-const SECRET_KEY =
-  "sk_test_51MDtzOBsZ5Deknzb8LN152zrBpi4lnoSksuzdy6ak2RW20VWkzhh4NjVuH96LQjsWJGbFwKU5VR5LMUHFjloD2YN00j5WbeAgF";
+const PUBLISHABLE_KEY = "YOUR PUBLIC KEY";
+const SECRET_KEY = "YOUR SECRET KEY";
 const Stripe = require("stripe");
 const stripe = Stripe(SECRET_KEY, { apiVersion: "2020-08-27" });
 router.post("/create-payment-intent", async (req, res) => {
